9399f6222ce24c1889b81097af031e03815cc25d
bennyp85 <benjamintp.1985@gmail.com>
2020-01-22 10:53:41 +1100

    Added bubble sort

--- Prac3/Program.cs
+++ Prac3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Resources;
 
@@ -11,15 +12,30 @@ namespace Prac3
             var watch = new Stopwatch();
             watch.Start();
             int counter = 1;
-            while(counter <= 1024)
+
+            while (counter <= 8)
             {
-                Console.WriteLine("Counter {0}", counter);
-                TestSumItem(counter);
-                counter *= 2;
+//                Console.WriteLine("Counter {0}", counter);
+//                TestSumItem(counter);
+//                
+//                
+//                counter *= 2;
+//                watch.Stop();
+//                Console.WriteLine(watch.ElapsedMilliseconds);
+
+                List<int> arr = new List<int>();
+
+                for (int i = 1; i <= counter; i++)
+                {
+                    arr.Add(i);
+                }
+
+                BubbleSort(arr);
                 watch.Stop();
-                Console.WriteLine(watch.ElapsedMilliseconds);
+                Console.WriteLine("{0} taken", watch.ElapsedMilliseconds);
+                counter *= 2;
             }
-            
+
             
         }
 
@@ -32,6 +48,30 @@ namespace Prac3
             }
 
             Console.WriteLine("{0}", sum);
+        }
+
+        static void BubbleSort(List<int> arr)
+        {
+            int n = arr.Count;
+                int t = 0;
+                for (int p = 0; p <= arr.Count - 2; p++)
+                {
+                    for (int i = 0; i <= arr.Count - 2; i++)
+                    {
+                        if (arr[i] > arr[i + 1])
+                        {
+                            t = arr[i + 1];
+                            arr[i + 1] = arr[i];
+                            arr[i] = t;
+                        }
+                    }
+                }
+
+                Console.WriteLine("{0} items", arr.Count);
+                foreach (var VARIABLE in arr)
+                {
+                    Console.WriteLine("{0}", VARIABLE);
+                }
         }
     }
 }
