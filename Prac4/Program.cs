093800242e77bf7e60311c657fe9baab73bf0b94
bennyp85 <benjamintp.1985@gmail.com>
2020-01-26 21:12:09 +1100

    Calculating powers with binary strings

---
+++ Prac4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.Serialization.Formatters;
+
+namespace Prac4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Pythagorian(2, 3);
+            Console.WriteLine(IsLeap(2008));
+        }
+
+        static void Pythagorian(int m, int n)
+        {
+            double a = Math.Abs(Math.Pow(m, 2) - Math.Pow(n, 2));
+            double b = 2 * m * n;
+            double c = Math.Pow(m, 2) + Math.Pow(n, 2);
+            Console.WriteLine("{0} {1} {2}", a, b, c );
+        }
+
+        static bool IsLeap(int year)
+        {
+            bool isLeap = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+            return isLeap;
+        }
+    }
+}
---
+++ Prac5/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Prac5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(Power(2, 3));
+            
+        }
+
+        static List<int> Reverse(List<int> list)
+        {
+            int size = list.Count;
+            List<int> items = new List<int>();
+            for (int i = 0; i < size; i++)
+            {
+                items.Add(list[i]);
+            }
+
+            items.Reverse();
+            return items;
+
+        }
+
+        static List<int> Binary(int n)
+        {
+            var binary = Convert.ToString(n, 2);
+            List<int> revBinary = new List<int>();
+            for (int i = binary.Length-1; i >= 0; i--)
+            {
+                int item = 0;
+                if (binary[i] == 48) item = 0;
+                else item = 1;
+                revBinary.Add(item);
+            }
+            return Reverse(revBinary);
+        }
+
+        static int Power(int b, int e)
+        {
+            List<int> revBinStr = Binary(e);
+            List<int> revList = new List<int>();
+
+            for (int i = 0; i < revBinStr.Count; i++)
+            {
+                revList.Add(revBinStr[i]);
+            }
+
+            int result = 1;
+            int idx = revList.Count - 1;
+
+            while (idx >= 0)
+            {
+                result *= result;
+                if (revList[idx] == 1)
+                {
+                    result *= b;
+                }
+
+                idx -= 1;
+            }
+
+            return result;
+        }
+    }
+}
