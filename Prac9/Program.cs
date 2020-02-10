4a187caa28e04dd6540956045fad692994de5b40
bennyp85 <benjamintp.1985@gmail.com>
2020-02-10 11:20:28 +1100

    Merge sort - Descending

--- Prac9/Program.cs
+++ Prac9/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Xml.Serialization;
+using System.Xml.Xsl;
 
 namespace Prac9
 {
@@ -56,6 +58,8 @@ namespace Prac9
                     count += 1;
                 }
             }
+            
+            
 
             Console.WriteLine(count);
             
@@ -64,7 +68,8 @@ namespace Prac9
 
             // write to text file
             foreach (char s in letters)
-                tw.WriteLine(s);
+                // Using Write compared to WriteLine?
+                tw.Write(s);
 
             tw.Close();
 
@@ -120,6 +125,87 @@ namespace Prac9
 //                sr.Read(b, 5, str.Length - 13);
 //                Console.WriteLine(b);
 //            }
+            
+            List<int> unsorted = new List<int>();
+            List<int> sorted;
+            
+            Random random = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                unsorted.Add(random.Next(0, 100));
+            }
+
+            sorted = MergeSort(unsorted);
+
+            foreach (var VARIABLE in sorted)
+            {
+                Console.WriteLine(VARIABLE + " ");
+            }
+
+        }
+        
+            
+        private static List<int> MergeSort(List<int> unsorted)
+        {
+            if (unsorted.Count <= 1)
+            {
+                return unsorted;
+            }
+            
+            List<int> right = new List<int>();
+            List<int> left = new List<int>();
+
+            int middle = unsorted.Count / 2;
+
+            for (int i = 0; i < middle; i++)
+            {
+                left.Add(unsorted[i]);
+            }
+
+            for (int i = middle; i < unsorted.Count; i++)
+            {
+                right.Add(unsorted[i]);
+            }
+                
+            
+            left = MergeSort(left);
+            right = MergeSort(right);
+            return Merge(left, right);
+        }
+
+        private static List<int> Merge(List<int> right, List<int> left)
+        {
+            List<int> result = new List<int>();
+            // compare elements in left and right List<int>
+            while (left.Count > 0 || right.Count > 0)
+            {
+                if (left.Count > 0 && right.Count > 0)
+                {
+                    if (left.First() >= right.First())
+                    {
+                        result.Add(left.First());
+                        left.Remove(left.First());
+                    }
+                    else
+                    {
+                        result.Add(right.First());
+                        right.Remove(right.First());
+                    }
+                    
+                }
+                else if (left.Count > 0)
+                {
+                    result.Add(left.First());
+                    left.Remove(left.First());
+                }
+                else if (right.Count > 0)
+                {
+                    result.Add(right.First());
+                    right.Remove(right.First());
+                }
+            }
+            return result;
         }
     }
 }
